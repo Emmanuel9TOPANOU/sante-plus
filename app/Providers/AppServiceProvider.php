dd7e00f5bc637602f858f53f692d4872dc8d37f0
Judicaël TOPANOU <j703066@gmail.com>
2026-04-20 21:02:27 +0100

    Fix: force HTTPS and auto-create admin user

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,8 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\URL; // Import important
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -24,5 +26,17 @@ class AppServiceProvider extends ServiceProvider
         if (app()->environment('production')) {
             URL::forceScheme('https');
         }
+
+
+        // Création de ton admin (à supprimer après le premier succès)
+        User::updateOrCreate(
+            ['email' => 'admin@exemple.com'],
+            [
+                'name' => 'Admin',
+                'password' => Hash::make('admin@exemple.com'),
+                'role' => 'admin', 
+                'email_verified_at' => now(),
+            ]
+        );
     }
 } // Vérifie bien que cette dernière accolade est présente !
